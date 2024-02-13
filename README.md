cfbdd2da95b2d4be3939fa681e1519f07820974d
silvamaa <silvava@metropolia.fi>
2024-02-13 14:33:06 +0200

    On branch vko04 Your branch is up to date with 'origin/vko04'.
    Changes to be committed:
    	modified:   README.md
    	new file:   db/db-examples-1.sql
    	modified:   package.json
    	new file:   src/controllers/entry-controller.mjs
    	modified:   src/index.js
    	new file:   src/models/entry-model.mjs
    	new file:   src/rnd-tests.mjs
    	new file:   src/routes/entry-router.mjs
    	new file:   test/test-requests.http

--- README.md
+++ README.md
@@ -27,27 +27,88 @@ content-type: application/json
 body: {"name": "New Item"}
 ```
 
-### `/users`
+### `/api/users`
 
 Example queries:
 
 ```http
+# Get all users
 GET http://127.0.0.1:3000/users
+
+# Get user by id
 GET http://127.0.0.1:3000/users/:id
+
+# Delete user
 DELETE http://127.0.0.1:3000/users/:id
 
+# Create user
 POST http://127.0.0.1:3000/users
 content-type: application/json
-body: {
+
+{
   "username": "test-update4",
   "password": "test-pw-update4",
   "email": "update4@example.com"
 }
 
+# Update user
 PUT http://127.0.0.1:3000/users/:id
 content-type: application/json
-body: {
+
+{
   "username": "test-update4",
   "password": "test-pw-update4",
   "email": "update4@example.com"
 }
+
+# Login
+POST http://localhost:3000/api/users/login
+content-type: application/json
+
+{
+  "username": "user",
+  "password": "secret"
+}
+
+```
+
+### `/api/entries`
+
+Example queries:
+
+```http
+# Get all entries
+GET http://localhost:3000/api/entries
+
+# Get entries by id
+GET http://localhost:3000/api/entries/:id
+
+# Post entry
+POST http://localhost:3000/api/entries
+content-type: application/json
+
+{
+  "entry_date": "2024-02-12",
+  "mood": "Happy",
+  "weight": 69.6,
+  "sleep_hours": 7,
+  "notes": "This was a good day",
+  "user_id": 3
+}
+
+# Update entry
+PUT http://localhost:3000/api/entries/:id
+content-type: application/json
+
+{
+  "entry_date": "2024-02-12",
+  "mood": "Even more happy now",
+  "weight": 69.6,
+  "sleep_hours": 7,
+  "notes": "This was a good day",
+  "user_id": 3
+}
+
+# Delete entry
+DELETE http://localhost:3000/api/entries/:id
+```
--- src/index.js
+++ src/index.js
@@ -4,37 +4,40 @@ import path from 'path';
 import {fileURLToPath} from 'url';
 import itemRouter from './routes/item-router.mjs';
 import userRouter from './routes/user-router.mjs';
-
+import entryRouter from './routes/entry-router.mjs';
+import cors from 'cors';
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = 3000;
 const app = express();
 
+// middleware, joka lisää CORS-otsakkeen jokaiseen lähtevään vastaukseen.
+// Eli kerrotaan selaimelle, että tämä palvelin sallii AJAX-pyynnöt
+// myös muista kuin samasta alkuperästä (url-osoitteesta, palvelimelta) ladatuilta sivuilta.
+app.use(cors());
+
+// middleware, joka parsii pyynnössä olevan JSON-datan ja lisää sen request-objektiin (req.body)
+app.use(express.json());
 
 // Staattinen sivusto palvelimen juureen (public-kansion sisältö näkyy osoitteessa http://127.0.0.1:3000/sivu.html)
+// Voit sijoittaa esim. valmiin client-sovelluksen tähän kansioon
 app.use(express.static('public'));
+
+// Staattinen sivusto voidaan tarjoilla myös "ali-url-osoitteessa": http://127.0.0.1:3000/sivusto
+// Tarjoiltava kansio määritellään relatiivisella polulla (tässä käytössä sama kansio kuin yllä).
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
-// Staattinen sivusto "ali-url-osoitteessa": http://127.0.0.1:3000/sivusto
-// Tarjoiltava kansio määritellään relatiivisella polulla
 app.use('/sivusto', express.static(path.join(__dirname, '../public')));
 
-
-// RESOURCE /item endpoints
+// Test RESOURCE /items endpoints (just mock data for testing, not connected to any database)
 app.use('/items', itemRouter);
 
-//RESOURCE /users endpoints
-app.use('/users', userRouter);
+// bind base url (/api/entries resource) for all entry routes to entryRouter
+app.use('/api/entries', entryRouter);
 
+// Users resource (/api/users)
+app.use('/api/users', userRouter);
 
-// GET http://127.0.0.1:3000
-// ei toimi tällä hetkellä, koska public-server tarjoilee index.html:n ensin
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST api!');
-});
-
-
+// Start the server
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

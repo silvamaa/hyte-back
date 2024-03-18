b962761ff146054f410cee82c9bb4edf6878b6ff
Silva Vähämaa <silva.maa44@gmail.com>
2024-03-18 13:04:08 +0200

    Changes to be committed: 	modified:   src/routes/entry-router.mjs

--- src/routes/entry-router.mjs
+++ src/routes/entry-router.mjs
@@ -17,9 +17,7 @@ entryRouter
   .get(authenticateToken, getEntries)
   .post(
     authenticateToken,
-    body('entry_date').isDate(),
     body('mood').optional().trim().isLength({min: 3, max: 20}).isString(),
-    body('weight').optional().isFloat({min: 30, max: 200}),
     body('sleep_hours').optional().isInt({min: 0, max: 24}),
     body('notes').optional().isString().isLength({min: 3, max: 300}),
     validationErrorHandler,
@@ -39,9 +37,7 @@ entryRouter
     param('id', 'must be integer').isInt(),
     // user_id is not allowed to be changed
     body('user_id', 'not allowed').not().exists(),
-    body('entry_date').optional().isDate(),
     body('mood').optional().trim().isLength({min: 3, max: 20}).isString(),
-    body('weight').optional().isFloat({min: 30, max: 200}),
     body('sleep_hours').optional().isInt({min: 0, max: 24}),
     body('notes').optional().isString().isLength({min: 3, max: 300}),
     validationErrorHandler,

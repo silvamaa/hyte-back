4974d1f0bf7cbdde6c36f520ac9ad931eac0f01c
Silva Vähämaa <silva.maa44@gmail.com>
2024-03-19 09:40:35 +0200

    Changes to be committed: 	modified:   db/create-db-user-private.sql

--- db/create-db-user-private.sql
+++ db/create-db-user-private.sql
@@ -1,3 +1,3 @@
-CREATE USER 'sil'@'hyte-server.northeurope.cloudapp.azure.com' IDENTIFIED BY 'hihi69';
-GRANT ALL PRIVILEGES ON `VitalTrack`.* TO 'sil'@'hyte-server.northeurope.cloudapp.azure.com';
+CREATE USER 'sil'@'localhost' IDENTIFIED BY 'hihi69';
+GRANT ALL PRIVILEGES ON `VitalTrack`.* TO 'sil'@'localhsot';
 FLUSH PRIVILEGES;
